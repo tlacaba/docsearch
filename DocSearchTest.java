39e83a588b2f26b581acd474c4e2fd3bc45470cc
tlacaba <timlacaba123@gmail.com>
2022-10-21 19:38:09 -0700

    added tests

---
+++ DocSearchTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DocSearchTest {
+    @Test
+    public void testHandleURL() {
+        
+    }
+}
+/*
+public class ListTests {
+    @Test
+    public void testFilter() {
+        List<String> input1 = new ArrayList<String>();
+        input1.add("The");
+        input1.add("Quick");
+        input1.add("Brown");
+        input1.add("Fox");
+        input1.add("jumped");
+        input1.add("over");
+        input1.add("the");
+        input1.add("Lazy");
+        input1.add("Dog.");
+        List<String> expected = new ArrayList<String>();
+        expected.add("The");
+        expected.add("Quick");
+        expected.add("Brown");
+        expected.add("Fox");
+        expected.add("Lazy");
+        expected.add("Dog.");
+        assertEquals(expected, ListExamples.filter(input1, new StartsWithCapital()));
+    }
+}
+*/
